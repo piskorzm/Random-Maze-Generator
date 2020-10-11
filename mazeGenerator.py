f2d223d941137fa02acc78d1b8229c73563c2fda
Marcin <marcinpiskorz01@gmail.com>
2020-10-11 19:03:34 +0200

    Pathfinding added

--- mazeGenerator.py
+++ mazeGenerator.py
@@ -1,10 +1,13 @@
 import pygame
 import random
 import time
+import copy
+
 pygame.init()
 white = (255,255,255)
 black = (0,0,0)
 red = (255,0,0)
+green = (0, 255, 0)
 
 window_w = 2000
 window_h = 1000
@@ -75,13 +78,13 @@ def occupied(position):
     return position[0] < 0 or position[0] >= rows or position[1] < 0 or position[1] >= cols or positions[position[0]][position[1]]
 
 def valid(position,neighbour):
-    up = [(position[0]),(position[1]-1)]
-    down = [(position[0]),(position[1]+1)]
-    left = [(position[0]-1),(position[1])]
-    right = [(position[0]+1),(position[1])]
+    up = [(position[0]-1),(position[1])]
+    down = [(position[0]+1),(position[1])]
+    left = [(position[0]),(position[1]-1)]
+    right = [(position[0]),(position[1]+1)]
     upleft = [(position[0]-1),(position[1]-1)]
-    upright = [(position[0]+1),(position[1]-1)]
-    downleft = [(position[0]-1),(position[1]+1)]
+    upright = [(position[0]-1),(position[1]+1)]
+    downleft = [(position[0]+1),(position[1]-1)]
     downright = [(position[0]+1),(position[1]+1)]
 
     if  position[0]<0 or position[0]>=rows or position[1]<0 or position[1]>=cols:
@@ -130,9 +133,76 @@ def generate_end():
         if (not occupied([end[0]-1, end[1]])):
             end[1] = 0
         else:
-            positions[end[0]][end[1]] == True
-    
+            positions[end[0]][end[1]] = True
 
+def explore(previous_position, current_position):
+
+    branch = [copy.copy(current_position)]
+
+    nexts = []
+
+    explored = False
+
+    is_correct = False
+
+    while(not explored):
+
+        
+            
+        if (current_position[0] == end[0]):
+            is_correct = True
+            explored = True
+            
+        else:
+
+            nexts = []
+
+            up = [(current_position[0]-1),(current_position[1])]
+            down = [(current_position[0]+1),(current_position[1])]
+            left = [(current_position[0]),(current_position[1]-1)]
+            right = [(current_position[0]),(current_position[1]+1)]
+
+            if (up != previous_position and positions_solve[up[0]][up[1]]):
+                nexts.append(up)
+            if (down != previous_position and positions_solve[down[0]][down[1]]):
+                nexts.append(down)
+            if (left != previous_position and positions_solve[left[0]][left[1]]):
+                nexts.append(left)
+            if (right != previous_position and positions_solve[right[0]][right[1]]):
+                nexts.append(right)
+
+            
+
+            if (len(nexts) == 0 or is_correct):
+                explored = True
+            elif (len(nexts) == 1):
+                previous_position = copy.copy(current_position)
+                current_position = nexts[0]
+                branch.append(copy.copy(current_position))
+
+                
+                
+                if (current_position[0] == end[0]):
+                    is_correct = True
+
+            else:
+                is_correct = explore(current_position, nexts[random.choice(range(len(nexts)))])
+
+    if (is_correct):
+        for position in branch:
+            path.append(position)
+    else:
+        for position in branch:
+            positions_solve[position[0]][position[1]] = False 
+    
+    return is_correct
+
+def print_path():
+    for position in path:
+        pygame.draw.rect(gameDisplay, red, [position[0]*block, position[1]*block, block, block])
+
+    
+    pygame.display.update()
 
 gameExit = False
 
@@ -143,6 +213,14 @@ make_maze()
 
 generate_end()
 print_maze()
+
+path = []
+positions_solve = copy.deepcopy(positions)
+explore(start, start1)
+
+print_path()
+
+
 
 print('Press q to quit')
 gameExit = False
